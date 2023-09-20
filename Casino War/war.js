f0d19620d817e8dcc1ab4586304e2fb9e3e19706
Mgphone <51868037+Mgphone@users.noreply.github.com>
2023-09-20 01:58:01 +0100

    change CSS and some layout

--- Casino War/war.js
+++ Casino War/war.js
@@ -1,7 +1,7 @@
 // var suits=["♠️","♥️","♣️","♦️"];
-var suits=["S","H","D","C"];
-var number=["A","K","Q","J","10","9","8","7","6","5","4","3","2"];
-var deck=[];
+var suits = ["S", "H", "D", "C"];
+var number = ["A", "K", "Q", "J", "10", "9", "8", "7", "6", "5", "4", "3", "2"];
+var deck = [];
 
 // for(i=0;i<suits.length;i++){
 //   // console.log(suits[i]);
@@ -24,38 +24,36 @@ var deck=[];
 //   {
 //     console.log("Dealer Win");
 //   }
-//   else 
+//   else
 //   console.log("Player Win");
 // }
 
-for(i=0;i<number.length;i++){
-  for(j=0;j<suits.length;j++){
-    deck.push(number[i]+suits[j]);
+for (i = 0; i < number.length; i++) {
+  for (j = 0; j < suits.length; j++) {
+    deck.push(number[i] + suits[j]);
   }
 }
-var dealerCard=deck[Math.floor(Math.random()*deck.length)];
-var playerCard=deck[Math.floor(Math.random()*deck.length)];
+var dealerCard = deck[Math.floor(Math.random() * deck.length)];
+var playerCard = deck[Math.floor(Math.random() * deck.length)];
 // var Dealercard=number[Math.floor(Math.random()*13)]+suits[Math.floor(Math.random()*4)];
 // var Playercard=number[Math.floor(Math.random()*13)]+suits[Math.floor(Math.random()*4)];
-$(".Dealer-Hit").on("click",function(){
-  $(".DealerCard").html("<img src=img/"+dealerCard+".png>");
+$(".Dealer-Hit").on("click", function () {
+  $(".DealerCard").html("<img src=img/" + dealerCard + ".png>");
 });
-$(".Player-Hit").on("click",function(){
-  $(".PlayerCard").html("<img src=img/"+playerCard+".png>"); 
+$(".Player-Hit").on("click", function () {
+  $(".PlayerCard").html("<img src=img/" + playerCard + ".png>");
   $(".winning").text(checkingValue());
 });
-$(".newGame").click(function(){
+$(".newGame").click(function () {
   location.reload();
 });
 
-function checkingValue(){
-  if (deck.indexOf(dealerCard)>deck.indexOf(playerCard)){
-    return("Player2Win");
-  }
-  else if(deck.indexOf(playerCard)>deck.indexOf(dealerCard)){
-    return("Player1Win");
-  }
-  else{
-      return("Draw")  ;
+function checkingValue() {
+  if (deck.indexOf(dealerCard) > deck.indexOf(playerCard)) {
+    return "Player 2 Win";
+  } else if (deck.indexOf(playerCard) > deck.indexOf(dealerCard)) {
+    return "Player 1 Win";
+  } else {
+    return "Draw";
   }
 }
